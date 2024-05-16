ddb543d22443f168187ec98e9fb2a2b5d6c81f9a
SabLuv <124561798+SabLuv@users.noreply.github.com>
2024-05-16 23:27:40 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let inputForm = document.querySelector(`#loginForm`)
+let buttonForm = document.querySelector(`#buttonForm`)
+let inputIN = document.querySelector(`#passwordInput`)
+let usersNode = document.querySelector(`#users`)
+let mainNode = document.querySelector(`.main`)
+
+clickFunc = () => {
+    for (let i = 0; i < 1000; i++) {
+        console.log('error')
+    }
+}
+
+usersNode.addEventListener(`click`, function() {
+    
+})
